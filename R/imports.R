26645ccac48ed2d219c81af908b39c5c080bdd8e
Stef van Buuren <stef.vanbuuren@tno.nl>
2017-07-07 16:21:27 +0200

    Update to R 3.0.4

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,8 @@
 #'@import shiny
 #'@importFrom MASS eqscplot
 #'@importFrom knitr knit2html
+#'@importFrom grDevices rgb xy.coords
+#'@importFrom graphics abline legend matlines matpoints
+#'                     polygon strwidth
+#'@importFrom stats na.omit rnorm t.test uniroot
 NULL
